9e29498929e0ad75f0731384a4c36f34d7379920
Mirkomilboy Sadirov <msadirov1997@gmail.com>
2021-06-01 12:58:04 +0500

    almost finito

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,12 @@ module.exports = {
       },
       backgroundColor: {
         'lilac': '#D8E3F5',
+        'lilac-100': '#FCFDFE',
         'lilac-300': '#F4F7FC',
         'lilac-400': '#F0F4FB',
+        'lilac-1000': '#D8E3F5',
         'purple': '#773DBD',
+        'main-light': '#D6D9DC',
         'main': '#202E3D',
         'card': '#374351',
       },
@@ -26,6 +29,7 @@ module.exports = {
         'lilac': '#FCFDFE',
         'lilacSecondary': '#D8E3F5',
         'lilac-500': '#ECF1FA',
+        'custom-gray-900': '#374351',
       },
       maxWidth: {
         '8xl': '84rem',
